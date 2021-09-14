54b9e48e06b069ec8a6e01ebe22d17144564ed51
zanama <57731386+zanmama@users.noreply.github.com>
2021-09-13 22:01:01 -0400

    webpack config details added

--- public/service-worker.js
+++ public/service-worker.js
@@ -43,13 +43,13 @@ self.addEventListener("install", function(evt) {
 
 
 self.addEventListener("fetch", function(evt) {
-    // cache successful requests to the API
+
     if (evt.request.url.includes("/api/")) {
       evt.respondWith(
         caches.open(DATA_CACHE_NAME).then(cache => {
           return fetch(evt.request)
             .then(response => {
-              // If the response was good, clone it and store it in the cache.
+              
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
@@ -57,7 +57,7 @@ self.addEventListener("fetch", function(evt) {
               return response;
             })
             .catch(err => {
-              // Network request failed, try to get it from the cache.
+              
               return cache.match(evt.request);
             });
         }).catch(err => console.log(err))
@@ -66,11 +66,9 @@ self.addEventListener("fetch", function(evt) {
       return;
     }
   
-    // if the request is not for the API, serve static assets using "offline-first" approach.
-    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
     evt.respondWith(
       caches.match(evt.request).then(function(response) {
         return response || fetch(evt.request);
       })
     );
-  });
+});
